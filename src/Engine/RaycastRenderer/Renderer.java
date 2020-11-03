1afb2783a8685ad930ef54b5476842087ef31fb9
logithss <47717097+logithss@users.noreply.github.com>
2020-11-02 21:49:39 -0500

    rendering implemented

--- src/Engine/RaycastRenderer/Renderer.java
+++ src/Engine/RaycastRenderer/Renderer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Engine.Renderer;
+package Engine.RaycastRenderer;
 
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
