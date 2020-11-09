f298fd3dcda2e632b315842a692a8162407c771e
IQ3-2020 <>
2020-11-09 12:14:57 -0500

    updated renderer

--- src/Engine/RaycastRenderer/Renderer.java
+++ src/Engine/RaycastRenderer/Renderer.java
@@ -30,7 +30,7 @@ public class Renderer {
     private GraphicsContext gc = frame.getGraphicsContext2D();
     
     private Point2D cam;
-    private double camA, fov;
+    private float camA, fov;
     
     public Renderer(){
         gc.setFill(Color.BLACK);
@@ -38,9 +38,9 @@ public class Renderer {
         gc.setFill(Color.GREEN);
         gc.fillRect(0, screenHeight/2, screenWidth, screenHeight/2);
         
-        cam = new Point2D(1.1, 1.1);
-        camA = 45.0+90.0;
-        fov = 60.0;
+        cam = new Point2D(1.5, 2.5);
+        camA = 0f;
+        fov = 60f;
     }
     
     //renders one frame
@@ -59,7 +59,7 @@ public class Renderer {
         offY = (int)Math.floor(cam.getY()-tileY);
         
         Point2D ray = new Point2D(cam.getX(), cam.getY());
-        double rayA = camA - (fov/2);
+        float rayA = camA - (fov/2);
         
         for(int r=0; r<screenWidth;r++){
             if(rayA < 0)  rayA+=360;
@@ -68,7 +68,7 @@ public class Renderer {
             boolean upward, rightward;
             upward = rayA<180; rightward = (rayA<90 || rayA>270);
             
-            final double tan = -Math.tan(Math.toRadians(rayA));
+            final double tan = Math.tan(Math.toRadians(rayA));
             final double cotan = 1/tan;
             
             double stepX=0, stepY=0;
@@ -78,11 +78,13 @@ public class Renderer {
             //intersects with vertical
             if(rightward){//looking right (+x)
                 rV = new Point2D(tileX+1, ray.getY()+(1-offX)*tan);
-                stepX = 1; stepY = tan;
+                stepX = 1; stepY = -tan;
+                if(upward) stepY = tan;
             }
             if(!rightward){//looking left (-x)
                 rV = new Point2D(tileX  , ray.getY()+(1-offX)*tan);
-                stepX = -1; stepY = tan;
+                stepX = -1; stepY = -tan;
+                if(upward) stepY = tan;
             }
             if(rayA == 90 || rayA ==270){//looking directly up or down
                 rV = new Point2D(ray.getX(), ray.getY());
@@ -93,43 +95,45 @@ public class Renderer {
                 int x = (int)Math.floor(rV.getX());
                 int y = (int)Math.floor(rV.getY());
                 if((x<0 || y<0) || (x>=mapX || y>=mapY)){ rV = Point2D.ZERO; break;}
-                if(map[x][y]>0){break;
-                }else rV.add(Math.floor(stepX), Math.floor(stepY));
+                if(map[y][x]>0){break;
+                }else rV = rV.add(stepX, stepY);
             }
             
             //intersects with horizontal
             if(upward){//looking up (+y)
                 rH = new Point2D(ray.getX()+(1-offY)*cotan, tileY+1);
-                stepX = cotan; stepY = 1;
+                stepX = -cotan; stepY = 1;
+                if(rightward) stepX = cotan;
             }
             if(!upward){//looking down (-y)
                 rH = new Point2D(ray.getX()+(1-offY)*cotan, tileY);
-                stepX = cotan; stepY = -1;
+                stepX = -cotan; stepY = -1;
+                if(rightward) stepX = cotan;
             }
             if(rayA == 0 || rayA == 180){//looking directly left or right
                 rH = new Point2D(ray.getX(), ray.getY());
                 stepX = 1; stepY = 0;
                 if(rayA == 180) stepX = -1;
             }
-            for(int i=0;i<16;i++){
+            for(int i=0;i<8;i++){
                 int x = (int)Math.floor(rH.getX());
                 int y = (int)Math.floor(rH.getY());
                 if((x<0 || y<0) || (x>=mapX || y>=mapY)){ rH = Point2D.ZERO; break;}
-                if(map[x][y]>0){break;
-                }else rV.add(Math.floor(stepX), Math.floor(stepY));
+                if(map[y][x]>0){break;
+                }else rH = rH.add(stepX, stepY);
             }
             
-            if(rH.magnitude()<rV.magnitude()){
+            if(rH.magnitude()<rV.magnitude() && !rH.equals(Point2D.ZERO)){
                 int x = (int)Math.floor(rH.getX());
                 int y = (int)Math.floor(rH.getY());
-                drawWallLine(r, rH.magnitude(), getColor(map[x][y]));
+                drawWallLine(r, rH.magnitude(), getColor(map[y][x]));
             }else{
                 int x = (int)Math.floor(rV.getX());
                 int y = (int)Math.floor(rV.getY());
-                drawWallLine(r, rV.magnitude(), getColor(map[x][y]).darker());
+                drawWallLine(r, rV.magnitude(), getColor(map[y][x]).darker());
             }
             
-            rayA += (double)(fov/screenWidth);
+            rayA += (fov/(float)screenWidth);
             
         }
     }
@@ -139,7 +143,7 @@ public class Renderer {
             case 0: return Color.RED;
             case 1: return Color.DIMGRAY;
             case 2: return Color.AQUA;
-            default: return Color.GREENYELLOW;
+            default: return Color.RED;
         }
     }
     private void drawWallLine(int x, double distance, Color color){
