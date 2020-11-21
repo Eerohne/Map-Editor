548e03b9e09b9de3bf4cac297fbc912e4b93736c
logithss <47717097+logithss@users.noreply.github.com>
2020-11-20 23:12:17 -0500

    Entity signal system done, changes to .lvl file layout

--- src/Engine/Entity/AbstractEntity/SpriteEntity.java
+++ src/Engine/Entity/AbstractEntity/SpriteEntity.java
@@ -1,19 +1,24 @@
 package Engine.Entity.AbstractEntity;
 
 import Engine.Core.Exceptions.EntityCreationException;
+import Engine.Util.RessourceManager.RessourceLoader;
 import java.util.HashMap;
 import javafx.geometry.Point2D;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 //Merouane Issad
 public abstract class SpriteEntity extends Entity{ //A type of entity that provides itself to the renderer, it is taken in consideration when rendering the game level (not the game UI)
     
+    public Image texture;
     public Color color; //temp code until we get sprites working, for now they only have a color
     
     public SpriteEntity(String name, Point2D position, Color color)
     {
         super(name, position);
         this.color = color;
+        
+        texture = new Image(RessourceLoader.ressourcePath+"images/sprite1.png", true);
     }
     
     public SpriteEntity(HashMap<String, Object> propertyMap)
@@ -21,6 +26,7 @@ public abstract class SpriteEntity extends Entity{ //A type of entity that provi
         super(propertyMap);
         
         //this.color = new Color(Float.parseFloat(propertyMap.get("col_r")), Float.parseFloat(propertyMap.get("col_g")), Float.parseFloat(propertyMap.get("col_b")), Float.parseFloat(propertyMap.get("col_a")));
+        texture = new Image(RessourceLoader.ressourcePath+"images/sprite1.png", true);
     }
     
     @Override
